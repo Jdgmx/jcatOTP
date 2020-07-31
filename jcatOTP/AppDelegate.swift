cde5057d6f0bb4d0eaf2845d52d49dea1f71c5bd
Joaquin Durand <joakod@yahoo.com>
2020-07-31 17:50:02 -0500

    Initial Commit

---
+++ jcatOTP/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  jcatOTP
+//
+//  Created by Joaquin Durand Gomez on 7/31/20.
+//  Copyright © 2020 jCat.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
