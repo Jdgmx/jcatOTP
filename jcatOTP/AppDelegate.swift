2e9557e54747373dbaa895fa185c63ac88c7ca77
Joaquin Durand <joakod@yahoo.com>
2020-08-01 10:43:05 -0500

    First commit.

--- jcatOTP/AppDelegate.swift
+++ jcatOTP/AppDelegate.swift
@@ -5,23 +5,23 @@
 //  Created by Joaquin Durand Gomez on 7/31/20.
 //  Copyright © 2020 jCat.io. All rights reserved.
 //
+// See: https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/SysServices/introduction.html
 
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
+class AppDelegate: NSObject, NSApplicationDelegate
+{
 	@IBOutlet weak var window: NSWindow!
 
 
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
+	func applicationDidFinishLaunching(_ aNotification: Notification)
+	{
 		// Insert code here to initialize your application
 	}
 
-	func applicationWillTerminate(_ aNotification: Notification) {
+	func applicationWillTerminate(_ aNotification: Notification)
+	{
 		// Insert code here to tear down your application
 	}
-
-
 }
-
